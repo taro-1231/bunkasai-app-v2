597413532a60bf082735d53f3fe9c9dd2c652b62
taro-1231 <okajimaryota1231@gmail.com>
2025-09-27 22:29:01 +0900

    nextconfigコメント追加

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   // ブラウザと別オリジンサーバーの通信はCORSにより制限される
   //フロントサーバとバックサーバはCORS制限を受けない
-  //rewriteはブラウザからnextのサーバーを経由することで同一ドメインでバックと通信することができる
+  //rewriteはブラウザからnextのサーバーを経由することで同一オリジンでバックと通信することができる
   //また、rewriteのsourceのURLでfetchできるため、バックエンドのフルURLを書く必要がない
   async rewrites() {
     return [
